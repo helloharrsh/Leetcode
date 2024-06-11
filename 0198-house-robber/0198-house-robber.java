4a6eef0ee7ddef02ebf5e10c1c18b86a27f31b82
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-11 19:33:26 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (64.84%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int helper(int idx, int[] dp, int[] arr){
+        if(idx < 0){
+            return 0;
+        }
+        if(idx == 0){
+            return arr[idx];
+        }
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        int pick = arr[idx] + helper(idx-2,dp,arr);
+        int not_pick = 0 + helper(idx-1,dp,arr);
+        
+        return dp[idx] = Math.max(pick,not_pick);
+    }
+    public int solve_memo(int[] nums){
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1); 
+        
+        return helper(n-1,dp,nums);
+        
+    }
+    public int rob(int[] nums) {
+        return(solve_memo(nums));
+    }
+}
