96610a9c104eda69bff4e968f2c12738f976bf31
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-11 10:03:52 +0530

    Time: 3 ms (44.47%), Space: 42.1 MB (32.74%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        List<Integer> remaining = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        
+        for(int val : arr2){
+        countMap.put(val,0);
+        }
+        
+        for(int value : arr1){
+            if(countMap.containsKey(value)){
+                countMap.put(value,countMap.get(value) +1);
+            }
+            else{
+                remaining.add(value);
+            }
+        }
+        
+        Collections.sort(remaining);
+        
+        for(int value: arr2){
+            for(int j = 0; j<countMap.get(value); j++){
+                result.add(value);
+            }
+        }
+        
+        result.addAll(remaining);
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
