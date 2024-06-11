379b3cc46a925f2870de56ad29277451b1a776ec
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-11 19:53:42 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (97.32%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    /*
     public int helper(int idx, int[] dp, int[] arr){
         if(idx < 0){
             return 0;
@@ -14,6 +16,7 @@ class Solution {
         
         return dp[idx] = Math.max(pick,not_pick);
     }
+    
     public int solve_memo(int[] nums){
         int n = nums.length;
         int[] dp = new int[n];
@@ -22,7 +25,34 @@ class Solution {
         return helper(n-1,dp,nums);
         
     }
+    */ 
+    
+    public int helper(int n, int [] dp, int[] nums){
+        dp[0] = nums[0];
+        
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int not_pick = dp[i-1];
+            
+            dp[i] = Math.max(pick, not_pick);
+        }
+        return dp[n-1];
+
+    }
+    
+    
+    public int solve_tabu(int [] nums){
+        int n = nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1); 
+        
+        return helper(n,dp,nums);
+    }
     public int rob(int[] nums) {
-        return(solve_memo(nums));
+        //return(solve_memo(nums));
+        return(solve_tabu(nums)); 
     }
 }
