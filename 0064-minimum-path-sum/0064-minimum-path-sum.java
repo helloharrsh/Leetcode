2ff818f1388ab209d4b0e676aa2571cef2dd3eee
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-12 18:57:09 +0530

    Time: 5 ms (12.12%), Space: 45.2 MB (68.35%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public int slove_memo(int i, int j ,int[][] matrix, int[][] dp){
         if(i == 0 && j ==0){
             return matrix[0][0];
@@ -15,17 +16,52 @@ class Solution {
         
         return dp[i][j] = Math.min(up,left); 
     }
+    
+    */
+    public int solve_tabu(int n, int m, int [][] matrix, int [][] dp){
+        for(int i = 0; i<n; i++){
+            for(int j =0; j<m; j++){
+                
+                if(i == 0 && j == 0){
+                    dp[i][j] = matrix[i][j];
+                }
+                else{
+                    int up = matrix[i][j];
+                    if(i>0){
+                        up += dp[i-1][j];
+                    }
+                    else{
+                        up += (int) Math.pow(10,9);
+                    }
+                    
+                    int left = matrix[i][j]; 
+                    if(j>0){
+                        left += dp[i][j-1];
+                    }
+                    else{
+                        left += (int) Math.pow(10,9);
+                    }
+                    
+                    dp[i][j] = Math.min(up,left);
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }
+    
     public int minPathSum(int[][] grid) {
         int n = grid.length; 
         int m = grid[0].length; 
         
         int[][] dp = new int[n][m];
         
+        /*
         for(int row[] : dp){
             Arrays.fill(row,-1);
         }
-        
         return slove_memo(n-1,m-1,grid,dp);
-        
+        */ 
+        return solve_tabu(n,m,grid,dp);
     }
 }
