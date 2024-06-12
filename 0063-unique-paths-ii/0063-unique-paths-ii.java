e7e0d6f891f5fd8ec3698a75150249033eb1c559
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-12 15:15:47 +0530

    Time: 1 ms (24.83%), Space: 41.6 MB (54.70%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public int solve_memo(int i, int j, int [][] maze, int[][] dp){
         if(i>=0 && j>=0 && maze[i][j] == 1){
             return 0;
@@ -22,6 +23,37 @@ class Solution {
         return dp[i][j] = up + left; 
     }
     
+    */
+    
+    public int solve_tabu(int n, int m ,int[][] maze, int [][] dp){
+        for(int i =0; i<n; i++){
+            for(int j =0; j <m; j++){
+                if(maze[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                if(i ==0 && j ==0){
+                    dp[i][j] = 1;
+                    continue; 
+                }
+                int up = 0; 
+                int left = 0; 
+                
+                if(i>0){
+                    up = dp[i-1][j];
+                }
+                
+                if(j>0){
+                    left = dp[i][j-1];
+                }
+                dp[i][j] = up + left;
+            }
+        }
+        
+        return dp[n-1][m-1]; 
+    }
+    
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n = obstacleGrid.length;
         int m = obstacleGrid[0].length; 
@@ -31,8 +63,8 @@ class Solution {
         for(int row[] :dp){
             Arrays.fill(row,-1); 
         }
-        
-        return solve_memo(n-1,m-1,obstacleGrid,dp); 
+        return solve_tabu(n,m,obstacleGrid,dp);
+        //return solve_memo(n-1,m-1,obstacleGrid,dp); 
         
     }
 }
