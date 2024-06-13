2a56a44b552b89d245e24ca4580a995ed92ee8b7
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-13 16:12:02 +0530

    Time: 45 ms (51.24%), Space: 44.8 MB (78.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    /*
     public static boolean solve_memo(int ind, int target, int [] arr, int[][] dp){
         if(target == 0){
             return true;
@@ -22,6 +24,32 @@ class Solution {
         return notTaken || taken;
     }
     
+    */ 
+    public boolean solve_tabu(int n, int k ,int[] arr)
+    {
+        boolean dp[][] = new boolean[n][k+1];
+        for(int i=0; i<n;i++){
+            dp[i][0] = true;
+        }
+        if(arr[0]<=k){
+            dp[0][arr[0]] = true;
+        }
+        
+        for(int ind =1; ind<n; ind++){
+            for(int target = 1; target <= k; target++){
+                boolean notTaken = dp[ind-1][target];
+                boolean taken = false;
+                
+                if(arr[ind] <= target){
+                    taken = dp[ind-1][target - arr[ind]];
+                }
+                
+                dp[ind][target] = notTaken || taken;
+            }
+        }
+        
+        return dp[n-1][k];
+    }
     
     public boolean canPartition(int[] nums) {
         int sum = 0;
@@ -30,12 +58,16 @@ class Solution {
         }
         if(sum%2==0){
             
+            /*
             int [][] dp = new int[nums.length][sum/2 +1];
             for(int row[] :dp){
                 Arrays.fill(row,-1);
             }
             
             return solve_memo(nums.length-1,sum/2, nums,dp);
+        */
+            
+            return solve_tabu(nums.length, sum/2,nums);
         }
         else{
             return false; 
