ed7d1bdcb957dd9d0f7eac2bf8605029ac501f53
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-17 14:56:12 +0530

    Time: 16 ms (61.45%), Space: 45.1 MB (21.49%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public int solve_memo(int[] arr, int ind, int T, int[][] dp){
         if(ind == 0){
             if(T%arr[0] == 0){
@@ -23,7 +24,47 @@ class Solution {
         return dp[ind][T] = Math.min(take,notTake);
     }
     
+    */
+    public int solve_tabu(int[] arr, int t){
+        int n = arr.length; 
+        int dp[][] = new int[n][t+1];
+        
+        int inf = (int) Math.pow(10,9);
+        
+        for(int i =0; i<=t; i++){
+            if(i%arr[0] == 0){
+                dp[0][i] = i/arr[0];
+            }
+            else{
+                dp[0][i] = inf;
+            }
+        }
+        
+        for(int ind = 1; ind <n; ind++){
+            for(int tar = 0; tar <= t; tar++){
+                
+                int notTake = 0 + dp[ind-1][tar];
+                int take = inf;
+                
+                if(arr[ind] <= tar){
+                    take = 1 + dp[ind][tar - arr[ind]];
+                }
+                
+                dp[ind][tar] = Math.min(notTake, take);
+            }
+        }
+        
+        int ans = dp[n-1][t];
+        
+        if(ans == inf){
+            return -1;
+        }
+        else{
+            return ans;
+        }
+    }
     public int coinChange(int[] coins, int amount) {
+        /*
         int n = coins.length; 
         int [][] dp = new int[n][amount+1];
         
@@ -40,5 +81,10 @@ class Solution {
             return ans;
         }
         
+    }
+    */
+        int [] arr =coins;
+        int t = amount;
+        return solve_tabu(arr,t);
     }
 }
