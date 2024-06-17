cd775de25ea541ae10cdbdc23f2341cd4a409ad4
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-17 19:46:58 +0530

    Time: 7 ms (50.11%), Space: 48 MB (17.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    /*
     public int solve_memo(int [] arr, int ind, int t, int[][] dp){
         if(ind == 0){
             if(t % arr[0] == 0)
@@ -17,8 +19,31 @@ class Solution {
         
         return dp[ind][t] = notTake + take;
     }
-    
+    */ 
+    public int solve_tabu(int t, int [] arr){
+        int n = arr.length;
+        
+        int dp[][] = new int[n][t+1];
+        
+        for(int i =0; i<= t; i++){
+            if(i%arr[0] == 0)
+                dp[0][i] = 1;
+        }
+        
+        for(int ind = 1; ind<n; ind++){
+            for(int tar =0; tar <= t; tar++){
+                int nottake = dp[ind-1][tar];
+                
+                int take = 0;
+                if(arr[ind] <= tar)
+                    take = dp[ind][tar-arr[ind]];
+                dp[ind][tar] = nottake + take;
+            }
+        }
+        return dp[n-1][t];
+    }
     public int change(int amount, int[] coins) {
+        /*
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
         
@@ -26,5 +51,8 @@ class Solution {
             Arrays.fill(row,-1);
         }
         return solve_memo(coins,n-1,amount,dp);
+        */ 
+        
+        return solve_tabu(amount,coins);
     }
 }
