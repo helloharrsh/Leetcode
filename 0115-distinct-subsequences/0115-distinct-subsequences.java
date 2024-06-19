56d8dc5169bc6871696852ff4e361fd918925fae
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-19 17:43:29 +0530

    Time: 20 ms (8.61%), Space: 49 MB (29.44%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public int solve_memo(String s1,String s2, int ind1, int ind2, int[][]dp){
         if(ind2<0)
             return 1;
@@ -16,12 +17,38 @@ class Solution {
         else
             return dp[ind1][ind2] = solve_memo(s1,s2,ind1-1,ind2,dp);
     }
+    */
+    
+    public int solve_tabu(String s1,String s2,int n, int m){
+        int dp[][] = new int[n+1][m+1];
+        
+        for(int i = 0; i< n+1; i++)
+            dp[i][0] = 1;
+        
+        for (int i = 1; i < m + 1; i++) {
+            dp[0][i] = 0;
+        }
+        
+        
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
+        /*
         int dp[][] = new int[n][m];
         for(int row[]:dp)
             Arrays.fill(row,-1);
         return solve_memo(s,t,n-1,m-1,dp);
+        */
+        return solve_tabu(s,t,n,m);
     }
 }
