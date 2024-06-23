02937b4a8140a8b52557e92c8afa7273de4e8019
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-23 15:41:54 +0530

    Time: 74 ms (24.73%), Space: 75.7 MB (19.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    /*
     public int solve_memo(int [] arr, int n, int ind, int prev, int[][] dp){
         if(ind == n)
             return 0;
@@ -13,13 +15,29 @@ class Solution {
         dp[ind][prev+1] = Math.max(notTake, take);
         return dp[ind][prev+1];
     }
-    
+    */
+    public int solve_tabu(int[] arr, int n){
+        int dp[][] = new int[n+1][n+1];
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int prev = ind-1; prev >= -1; prev --){
+                int notTake = 0 + dp[ind+1][prev+1];
+                int take = 0;
+                if(prev == -1 || arr[ind] > arr[prev])
+                    take = 1 + dp[ind+1][ind+1];
+                dp[ind][prev+1] = Math.max(notTake, take);
+            }
+        }
+        return dp[0][0];
+    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+        /*
         int dp[][] = new int[n][n+1];
         for(int row[] :dp)
             Arrays.fill(row,-1);
         
         return solve_memo(nums,n,0,-1,dp);
+        */
+        return solve_tabu(nums,n);
     }
 }
